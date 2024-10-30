f55a3a022f31c1ba4b4d02867b50dca3de706fc7
kingrangE <yuvw123@gmail.com>
2024-10-30 10:49:53 +0900

    고득점 kit 주식 가격 문제풀이

--- programmers/src/stack_queue/stock_price/Main.java
+++ programmers/src/stack_queue/stock_price/Main.java
@@ -1,4 +1,25 @@
 package stack_queue.stock_price;
-
+import java.util.*;
 public class Main {
+    public static void main(String[] args) {
+        int[] prices = {2,3,5,6,1};
+        System.out.println(Arrays.toString(solution(prices)));
+    }
+    public static int[] solution(int[] prices) {
+        List<Integer> result = new LinkedList<>();
+        loop_i:
+        for(int i = 0; i<prices.length; i++){
+            int time = -1;
+            loop_j:
+            for(int j = i; j<prices.length; j++){
+                time++;
+                if(prices[i] > prices[j]){
+                    result.add(time);
+                    continue loop_i;
+                }
+            }
+            result.add(time);
+        }
+        return result.stream().mapToInt(e->e).toArray();
+    }
 }
