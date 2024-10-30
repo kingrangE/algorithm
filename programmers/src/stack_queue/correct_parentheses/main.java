74e2f8387402e3daf0ec6d9be768b97a89099691
kingrangE <yuvw123@gmail.com>
2024-10-30 10:48:58 +0900

    고득점 kit 올바른 괄호 문제 풀이

--- programmers/src/stack_queue/correct_parentheses/main.java
+++ programmers/src/stack_queue/correct_parentheses/main.java
@@ -1,4 +1,33 @@
 package stack_queue.correct_parentheses;
 
+import java.util.*;
+
 public class main {
+    public static void main(String[] args) {
+        String s ="(())()";
+        String s2 = "()()";
+        String s3= "(()(";
+        System.out.println(solution(s3));
+    }
+    static boolean solution(String s) {
+        if (s.charAt(0) == ')') return false;
+        String[] arrayStr = s.split("");
+        Stack<String> stack = new Stack<>();
+
+        try{
+            for(String str : arrayStr) {
+                if(str.equals("(")) {
+                    stack.push(str);
+                }
+                else if(str.equals(")")) {
+                    stack.pop();
+                }
+            }
+        }catch(Exception e){
+            return false;
+        }
+
+
+        return stack.isEmpty();
+    }
 }
